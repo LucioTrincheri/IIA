797b7c9c24cff23b47039ef9446e457fd264f7e7
LucioTrincheri <lucio4852071@gmail.com>
2022-07-09 15:03:41 -0300

    Added complete course

---
+++ TP2_Busqueda/search.py
@@ -0,0 +1,150 @@
+# Ignacio Petruskevicius y Lucio Trincheri
+
+# search.py
+# ---------
+# Licensing Information: Please do not distribute or publish solutions to this
+# project. You are free to use and extend these projects for educational
+# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
+# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+
+"""
+In search.py, you will implement generic search algorithms which are called
+by Pacman agents (in searchAgents.py).
+"""
+
+import util
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples,
+        (successor, action, stepCost), where 'successor' is a
+        successor to the current state, 'action' is the action
+        required to get there, and 'stepCost' is the incremental
+        cost of expanding to that successor
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.  The sequence must
+        be composed of legal moves
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other
+    maze, the sequence of moves will be incorrect, so only use this for tinyMaze
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s,s,w,s,w,w,s,w]
+
+def search(problem, fringe):
+    initial_state = problem.getStartState()
+    initial_actions = []
+    initial_candidate = (initial_state, initial_actions)
+    fringe.push(initial_candidate)
+    closed_set = set()
+    while not fringe.isEmpty():
+        candidate = fringe.pop()
+        state, actions = candidate
+        if problem.isGoalState(state):
+            return actions
+        if state not in closed_set:
+            closed_set.add(state)
+            candidate_successors = problem.getSuccessors(state)
+            candidate_successors = filter(lambda x: x[0] not in closed_set, candidate_successors)
+            candidate_successors = map(lambda x: (x[0], actions + [x[1]]), candidate_successors)
+            for candidate in candidate_successors:
+                fringe.push(candidate)
+
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first
+
+    Your search algorithm needs to return a list of actions that reaches
+    the goal.  Make sure to implement a graph search algorithm
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
+    return search(problem, util.Stack())
+
+
+def breadthFirstSearch(problem):
+    """
+    Search the shallowest nodes in the search tree first.
+    """
+    return search(problem, util.Queue())
+
+def uniformCostSearch(problem):
+    "Search the node of least total cost first."
+    return search(problem, util.PriorityQueueWithFunction(lambda candidate : problem.getCostOfActions(candidate[1])))
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """
+    Search the node that has the lowest combined cost and heuristic first.
+
+    Aca se presentan dos soluciones muy similares:
+        - La primera toma los nodos ordenados por prioridad f() en el orden que fueron insertados.
+          Esto causa que el sistema en algunos casos sea mejor o peor ya que tendra la tendencia que a mismos
+          costos siga siempre en una direccion en particular. En nuestro caso WEST EAST SOUTH NORTH.
+          Esta heuristica causo un valor menor de nodos expandidos en el ejemplo bigMaze pero puede llegar
+          a tener un peor rendimiento en algunos laberintos (mayor cantidad de nodos expandidos)
+        - La segunda heuristica funciona al igual que la primera solo que toma en cuenta la antiguedad de los nodos
+          insertados en los candidatos a expandir. Esto causa que el resultado no prefiera expandirse en una sola
+          direccion sino que sigue mas de cerca a la idea de astar.
+
+        Se puede cambiar el orden de los returns para ejecutar una o la otra y ver las diferencias en los nodos expandidos.
+    """
+    return search(problem, util.PriorityQueueWithFunction(lambda candidate : heuristic(candidate[0], problem) + problem.getCostOfActions(candidate[1])))
+    return search(problem, util.PriorityQueueWithFunction(lambda candidate : heuristic(candidate[0], problem) + problem.getCostOfActions(candidate[1]) + len(candidate[1]) / float((problem.walls.height - 2) * (problem.walls.width - 2))))
+
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
